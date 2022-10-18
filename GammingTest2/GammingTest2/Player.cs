fd2fd244e7a6827759a52c80f58f11cdfa2479ef
Henrique <80294621+HenriqueM-1259@users.noreply.github.com>
2022-10-18 19:02:18 -0300

    fisrt commit

---
+++ GammingTest2/GammingTest2/Player.cs
@@ -0,0 +1,69 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GammingTest2
+{
+    internal class Player
+    {
+        public Player(RenderWindow win)
+        {
+            this.win = win;
+        }
+        public RenderWindow  win { get; set; }
+        private Vector2f Position { get; set; } = new Vector2f(100,100);
+        private Vector2f Tamanho { get; set; } = new Vector2f(25, 40);
+        private RectangleShape rectangleShape { get; set; }
+
+        public void SetPosition(float? x, float? y)
+        {
+            Vector2f p = Position;
+            p.X += x.Value;
+            p.Y += y.Value + 2;
+            Position = p;
+        }
+
+        public RectangleShape CriarPersonagem()
+        {
+            rectangleShape = new RectangleShape();
+            rectangleShape.Size = Tamanho;
+            rectangleShape.Position = Position;
+            rectangleShape.FillColor = Color.Green;
+            return rectangleShape;
+        }
+
+        public void Drawn()
+        {
+            win.Draw(CriarPersonagem());
+        }
+
+        public void Update()
+        {
+            if (Keyboard.IsKeyPressed(Keyboard.Key.D))
+            {   
+                if(Position.X <= win.Size.X - 25)
+                SetPosition(0.1f,0);
+            }
+            else if (Keyboard.IsKeyPressed(Keyboard.Key.A))
+            {
+                if (Position.X >= 0)
+                    SetPosition(-0.1f, 0);
+            }
+            else if (Keyboard.IsKeyPressed(Keyboard.Key.W))
+            {
+                if(Position.Y >= 0)
+                SetPosition(0, -0.1f);             
+            }
+            else if (Keyboard.IsKeyPressed(Keyboard.Key.S))
+            {
+                if (Position.Y <= win.Size.Y - 35)
+                    SetPosition(0, 0.1f);
+            }
+        }
+
+
+    }
+}
---
+++ GammingTest2/GammingTest2/Program.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualBasic;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace GammingTest2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            ViewWindow viewWindow = new ViewWindow();
+            viewWindow.Run();
+
+        }
+       
+    }
+
+
+    class Frutas
+    {
+        public Vector2f posicao { get; set; }
+        public float width { get; set; } = 20f;
+        public float height { get; set; } = 20f;
+
+        
+        public void RandLocation()
+        {
+            int x = RandomNumber(50, 500);
+            int y = RandomNumber(50, 500);
+            posicao =  new Vector2f(x, y);
+        }
+        
+        private int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+        public void removeFruta()
+        {
+            this.posicao = new Vector2f(0,0);
+        }
+
+        public RectangleShape FrutaDrawn()
+        {
+
+
+            /* RectangleShape playerDraw = new RectangleShape();
+             playerDraw.Size = new Vector2f(width, height);
+             playerDraw.Position = new Vector2f(this.x,this.y);
+             playerDraw.FillColor = Color.Yellow;*/
+
+
+            RectangleShape frutaDrawn = new RectangleShape(new Vector2f(width, height))
+            {
+
+                FillColor = Color.Black,
+                Position = posicao
+
+             };
+            if (frutaDrawn.Position == new Vector2f(0,0))
+           {
+                RandLocation();
+           }          
+            return frutaDrawn;
+        }
+    }
+    
+    class ViewWindow
+    {
+        public void Run()
+        {
+            RenderWindow renderWindow;
+            renderWindow = new RenderWindow(new VideoMode(860, 500), "First window");
+            renderWindow.Closed += (_, __) => renderWindow.Close();
+            Console.WriteLine("Iniciado...");
+
+
+            Player player = new Player(renderWindow);
+          
+            
+            while (renderWindow.IsOpen)
+            {
+                renderWindow.DispatchEvents();
+                renderWindow.Clear(Color.White);
+
+                player.Drawn();
+                player.Update();
+
+
+
+                renderWindow.Display();
+            }
+
+
+        }
+        
+    }
+}
